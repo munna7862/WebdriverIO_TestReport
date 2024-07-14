323d230b0731ac96834e238e6902fc97c3ab7f32
munna7862 <sadhik7862@gmail.com>
2024-07-14 18:10:21 +0530

    12

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -4,12 +4,13 @@ import SecurePage from '../pageobjects/secure.page.js'
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
-        await LoginPage.open()
+        console.log("123")
+        // await LoginPage.open()
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-        await expect(SecurePage.flashAlert).toBeExisting()
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!')
+        // await LoginPage.login('tomsmith', 'SuperSecretPassword!')
+        // await expect(SecurePage.flashAlert).toBeExisting()
+        // await expect(SecurePage.flashAlert).toHaveTextContaining(
+        //     'You logged into a secure area!')
     })
 })
 
