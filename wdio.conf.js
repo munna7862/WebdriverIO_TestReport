4bdc946f89cb4fead06b917f4ee7ea4f8891dd25
munna7862 <sadhik7862@gmail.com>
2024-07-15 17:15:22 +0530

    wdio

--- wdio.conf.js
+++ wdio.conf.js
@@ -50,7 +50,11 @@ export const config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        acceptInsecureCerts: true,
+        'goog:chromeOptions':{
+            args: ['--headless', '--disable-gpu', '--disable-dev-shm-usage']
+        }
     }],
 
     //
